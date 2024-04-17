a4e2ea1553de6b4fa494ce73c2558f525a6ae2e3
Chanwon-Seo <scwonn60@gmail.com>
2024-04-17 19:50:28 +0900

    memberModel.css, houseListBtn.js remove

--- js/houseListBtn.js
+++
@@ -1,7 +0,0 @@
-$(".houseListBtn").click(function () {
-  if ($(".houseList").css("display") === "none") {
-    $(".houseList").css("display", "flex");
-  } else {
-    $(".houseList").css("display", "none");
-  }
-});
