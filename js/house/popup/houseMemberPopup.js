8bfd53f9d8ae300a49c198316bdbeee2e075fbb3
dmsrb0125 <52693374+dmsrb0125@users.noreply.github.com>
2024-04-18 20:06:50 +0900

    refactor: 인원추가, 수정 부분 디자인 수정및 수정 부분  유효성검사 추가

--- js/house/popup/houseMemberPopup.js
+++ js/house/popup/houseMemberPopup.js
@@ -1,16 +1,21 @@
 $(function () {
-    // $("#confirm").click(function () {
-    //     modalClose();
-    //     //컨펌 이벤트 처리
-    // });
-    $("#modal-open").click(function () {
-        $("#popup").css('display', 'flex').hide().fadeIn();
-    });
-    $("#close").click(function () {
-        modalClose();
-    });
+  $("#modal-open").click(function () {
+    $("#popup").css("display", "flex").hide().fadeIn();
+  });
 
-    function modalClose() {
-        $("#popup").fadeOut();
-    }
+  $("#close").click(function () {
+    $("#memberName").val("");
+    $("#memberImage").val("");
+    $("#memberAdvantages").val("");
+    $("#memberStyle").val("");
+    $("#memberBlogUrl").val("");
+    $("#memberMBTI").val("");
+    $("#memberSummary").val("");
+
+    modalClose();
+  });
+
+  function modalClose() {
+    $("#popup").fadeOut();
+  }
 });
